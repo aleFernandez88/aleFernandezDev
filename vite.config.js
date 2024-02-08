03b348f5c2ae430f5906aae474720c207f380432
Ale Fernandez <alejandro.d.fernandez.88@gmail.com>
2024-02-08 00:50:31 -0300

    HTTPS

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"http://www.alefernandez.site"
+  base:"https://www.alefernandez.site"
 })
